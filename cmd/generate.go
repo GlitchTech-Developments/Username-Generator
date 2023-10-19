253284e7d61cd98c16418379dbee5b42f41771cc
JesseKoldewijn <dev@glitchtech.eu>
2023-10-19 18:38:42 +0200

    feat: working uName gen flow with flags

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,3 +1,6 @@
+/*
+Copyright © 2023 GlitchTech Developments <dev@glitchtech.eu>
+*/
 package cmd
 
 import (
@@ -19,40 +22,36 @@ var generateCmd = &cobra.Command{
 }
 
 func generateUsername(cmd *cobra.Command, args []string) {
-	minLengthArg, _ := cmd.Flags().GetInt("min")
-	maxLengthArg, _ := cmd.Flags().GetInt("max")
-	wordCountArg, _ := cmd.Flags().GetInt("words")
-	separatorArg, _ := cmd.Flags().GetString("separator")
-	includeNumbersArg, _ := cmd.Flags().GetBool("numbers")
-	numbersCountArg, _ := cmd.Flags().GetInt("numbers-count")
-
-	// Set the min and max length of the username with fallback
-	minLength := minLengthArg | 6
-	maxLength := maxLengthArg | 12
-	wordCount := wordCountArg | 2
-	separator := separatorArg
-
-	if separator == "" {
-		separator = "-"
-	}
+	minLength, _ := cmd.Flags().GetInt("min")
+	maxLength, _ := cmd.Flags().GetInt("max")
+	wordCount, _ := cmd.Flags().GetInt("words")
+	separator, _ := cmd.Flags().GetString("separator")
+	includeNumbers, _ := cmd.Flags().GetBool("numbers")
 
 	// Initial string to append to
 	initialString := ""
-
-	// Loop 20 times
 	for i := 0; i < wordCount; i++ {
 		// Generate a fake word
 		fakeWord := fakelish.GenerateFakeWord(minLength, maxLength)
 		// Capitalize the first letter
 		fakeWord = cases.Title(language.AmericanEnglish).String(fakeWord)
-		// append the fake word
-		initialString += fakeWord
+		
+		// If it's not the first or last word
+		if i != 0 {
+			
+			// Append the separator
+			initialString += separator
+			// Append the fake word
+			initialString += fakeWord
+		} else {
+			initialString += fakeWord
+		}
 	}
 
 	// If the user wants numbers
-	if includeNumbersArg {
+	if includeNumbers {
 		// Loop through the numbers count
-		for i := 0; i < numbersCountArg; i++ {
+		for i := 0; i < 4; i++ {
 			// Create ioReader
 			ioReader := rand.Reader
 			// Random integer between 0 and 9
@@ -62,9 +61,14 @@ func generateUsername(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	fmt.Println(initialString)
+	fmt.Println("Done! Your generated username is: ", initialString)
 }
 
 func init() {
 	rootCmd.AddCommand(generateCmd)
+	generateCmd.Flags().IntP("min", "m", 6, "Minimum length of the username")
+	generateCmd.Flags().IntP("max", "M", 12, "Maximum length of the username")
+	generateCmd.Flags().IntP("words", "w", 2, "Number of words to generate")
+	generateCmd.Flags().StringP("separator", "s", "", "Separator between words")
+	generateCmd.Flags().BoolP("numbers", "n", false, "Include numbers in the username")
 }
