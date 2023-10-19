f665ea248a69c91ae597eb514b51a7817315f897
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-10-19 08:36:16 +0200

    fix: added ignore file, cmd desc (minimal-base) and cobra flag

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ Copyright © 2023 GlitchTech Developments <dev@glitchtech.eu>
 */
 package main
 
-import "github.com/GlitchTech-Developments/Username-Generator/cmd"
+import (
+	"github.com/GlitchTech-Developments/Username-Generator/cmd"
+	"github.com/spf13/cobra"
+)
 
 func main() {
+	cobra.EnableCaseInsensitive = true
 	cmd.Execute()
 }
