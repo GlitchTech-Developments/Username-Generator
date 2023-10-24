6952acff237337d40b330458b26b2b5433cf67e2
JesseKoldewijn <dev@glitchtech.eu>
2023-10-24 21:54:41 +0200

    fix: embedding version string

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ Copyright © 2023 GlitchTech Developments <dev@glitchtech.eu>
 package main
 
 import (
+	"embed"
+
 	"github.com/GlitchTech-Developments/Username-Generator/cmd"
 )
 
+//go:embed current-version.txt
+var f embed.FS
+
 func main() {
-	cmd.Execute()
+	version, _ := f.ReadFile("current-version.txt")
+
+	cmd.Execute(string(version))
 }
