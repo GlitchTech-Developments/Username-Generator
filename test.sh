b46a2662e6a70b9583bee61b2ab0e22f6bbdcf23
JesseKoldewijn <dev@glitchtech.eu>
2023-10-25 00:11:20 +0200

    cleanup: removed test script for regex check in CI

--- test.sh
+++
@@ -1,5 +0,0 @@
-if [[ "\"${{ steps.vars.outputs.tag }}\"" =~ ^v[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
-    echo "String matches the pattern 'vX.X.X' or 'vX.X'."
-else
-    echo "String does not match the pattern 'vX.X.X' or 'vX.X'."
-fi
